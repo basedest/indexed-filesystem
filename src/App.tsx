3af46f9d00537a9a39919eb33ba534a59a377bd7
Ivan Shcherbakov <shcherbakov@trueconf.ru>
2023-04-11 18:04:47 +0300

    release@0.1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useState } from "react";
-import DirectoryView from "./Directory";
+import DirectoryView from "./components/Directory";
 import IDBManager from "./features/idb/IDBManager";
 
 const buttonClasses = "bg-blue-600 hover:bg-blue-700 transition hover:scale-105 text-white py-2 px-4 rounded-xl mb-4 mx-auto block"
@@ -8,6 +8,7 @@ function App() {
   const [directoryHandle, setDirectoryHandle] = useState<null | FileSystemDirectoryHandle>(null);
   const [idb, setIdb] = useState<IDBManager | null>(null);
   const [popUp, setPopUp] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -23,18 +24,32 @@ function App() {
   }, [])
   
   const onPopUpClicked = useCallback(async () => {
-    // @ts-ignore
-    await directoryHandle.requestPermission({mode: 'readwrite'});
-    setPopUp(false);
-  }, [directoryHandle])
+    try {
+      // @ts-ignore
+      const permission = await directoryHandle.requestPermission({mode: "readwrite"});
+      if (permission !== "granted") {
+        throw new Error("Permission was not granted");
+      }
+    } catch (e: unknown) {
+      console.error(e);
+      setError(true);
+    } finally {
+      setPopUp(false);
+    }
+  }, [directoryHandle]);
 
   function onChooseDirectoryClicked() {
+    // @ts-ignore
     window.showDirectoryPicker({type: "openDirectory"})
-    .then(handle => {
-      setDirectoryHandle(handle as FileSystemDirectoryHandle);
-      idb!.setRoot(handle as FileSystemDirectoryHandle);
+    .then((handle: FileSystemDirectoryHandle) => {
+      setDirectoryHandle(handle);
+      idb!.setRoot(handle);
+      setError(false);
     })
-    .catch(console.error)
+    .catch((e:Error) => {
+      console.error(e);
+      setError(true);
+    })
   }
 
   return (
@@ -53,7 +68,14 @@ function App() {
           Set new root
         </button>
       }
-        {(directoryHandle && !popUp) && <DirectoryView directoryHandle={directoryHandle} />}
+      {
+        error ?
+        <section className="flex flex-col items-center">
+          <h1 className="text-lg font-medium text-red-600">Oops! Seems like there's an error</h1>
+          <p className="text-gray-600">See logs to find the issue</p>
+        </section>
+        : (directoryHandle && !popUp) && <DirectoryView directoryHandle={directoryHandle} />
+      }
       </div>
     </main>
   );
